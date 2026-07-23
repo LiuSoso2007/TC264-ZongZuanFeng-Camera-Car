2ebbf39580b70659bd700c3a478b99b37332c093
Liusoso2007 <2751291976@qq.com>
2026-07-23 18:20:10 +0800

    入环正在优化+低延迟屏幕+白底黑字

--- Seekfree_TC264_Opensource_Library/user/cpu0_main.c
+++ Seekfree_TC264_Opensource_Library/user/cpu0_main.c
@@ -63,8 +63,7 @@ int core0_main(void)
     cpu_wait_event_ready();
 
 #if IPS200_TEXT_DISPLAY_ENABLE
-    ips200_full(RGB565_BLACK);  /* ÇåÆÁÎªºÚÉ« */
-    ips200_set_color(RGB565_WHITE, RGB565_BLACK);
+    ips200_set_color(RGB565_BLACK, RGB565_WHITE); /* ºÚ×Ö°×µ× */
     ips200_show_string(2U, 128U, "L_Enc:");
     ips200_show_string(2U, 144U, "R_Enc:");
     ips200_show_string(2U, 170U, "Err:");

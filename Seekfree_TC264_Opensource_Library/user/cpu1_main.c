022a318887652da2894e00d4e494488cd53c8162
Liusoso2007 <2751291976@qq.com>
2026-08-13 17:53:36 +0800

    fix: 降低弯道帧间微分摆动

--- Seekfree_TC264_Opensource_Library/user/cpu1_main.c
+++ Seekfree_TC264_Opensource_Library/user/cpu1_main.c
@@ -42,7 +42,7 @@ static int16_t  EncCount        = 0;
 
 /* PD鍙傛暟 */
 #define PD_KP          0.78f
-#define PD_KD          1.0f  /* 降低帧间Err跳变的随机微分冲击，保留快速入弯预判。 */
+#define PD_KD          0.8f  /* D仅修正帧间趋势，避免整数Err波动造成舵机左右甩动。 */
 
 /* 左右电机PI控制器。 */
 static PI_t s_PI_Left, s_PI_Right;
